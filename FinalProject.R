b2c4099b980fbb8fae6aa69307d9b69167013c4e
lilybate <121913039+lilybate@users.noreply.github.com>
2023-03-05 21:05:27 -0800

    Changes to my tab and chart

--- FinalProject.R
+++ FinalProject.R
@@ -3,6 +3,8 @@ library(shiny)
 library(tidyverse)
 library(ggplot2)
 
+data <- read_delim("Rail_Equipment_Accident_Incident_Data.csv")
+
 ui <- fluidPage(
     titlePanel("Info 201 Final project"),
     tabsetPanel(
@@ -21,10 +23,23 @@ ui <- fluidPage(
   ),
   
   tabPanel(
-    "Lily's Panel",
+    "Lily's Panel - Weather and Visibility Trends",
+    sidebarLayout(
+      sidebarPanel(
+        p("Observe the effect on weather on incidents."),
+        radioButtons("weather", 
+                     "Choose weather type to veiw",
+                           choices = unique(data$`Weather Condition`),
+                           selected = "Clear"),
+      radioButtons("vis_button",
+       "Display time of day accident occured?",
+        choices = c("Yes", "No"),
+        selected = "No")
+      ),
     mainPanel(
-      "Lily's project here"
-    )
+      plotOutput("weather_graph"),
+      textOutput("weatherobs")
+    ))
   ), 
   
   tabPanel(
@@ -44,8 +59,33 @@ ui <- fluidPage(
 
 
 server <- function(input, output) {
+  
+weathergraph <- reactive({
+  data %>% 
+    filter(`Weather Condition` %in% input$weather) %>% 
+    filter(`Report Year` == 2022)
+})
 
+output$weather_graph <- renderPlot ({
+  if(input$vis_button == "Yes")({
+    ggplot(weathergraph(), (aes (y= `Accident Type`, fill = factor(Visibility))))+
+      geom_histogram(stat = "count")
+  })else ({
+    ggplot(weathergraph(), (aes (y= `Accident Type`)))+
+    geom_histogram(stat = "count")
+    })
+})
+
+output$weatherobs <- renderPrint({
+  data %>% 
+    filter(`Weather Condition` %in% input$weather) %>% 
+    filter(`Report Year` == 2022) %>%  
+    nrow() %>% 
+    p("There were", . , "total collisions in the selected weather type.")
+})
 }
 
 
 shinyApp(ui = ui, server = server)
+
+
